17c7bddf073ad75c2c93160d7f38135f21aea187
Christopher Lien <cnlien@gmail.com>
2019-09-30 22:26:16 -0400

    working on .map

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import CharacterInfo from './components/characterInfo.js';
-// import CharacterGrid from './components/characterGrid.js';
 
-const App = () => {
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a side effect in a component, you want to think about which state and/or props it should sync up with, if any.
@@ -12,7 +11,6 @@ const App = () => {
     <div className="App">
       <h1 className="Header">React Wars</h1>
       <CharacterInfo />
-      {/* <CharacterGrid /> */}
     </div>
   );
 }
--- starwars/src/components/characterInfo.js
+++ starwars/src/components/characterInfo.js
@@ -6,19 +6,53 @@ import './StarWars.css'
 const CharacterInfo = () => {
   const [charData, setCharData] = useState('');
 
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const charList = await axios.get(
+  //         'https://swapi.co/api/people/'
+  //       );
+  //       setCharData(charList.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   fetchData();
+  // },[]);
   useEffect(() => {
-      axios.get('https://swapi.co/api/people/1')
-      .then (res => {
-        let characters = res.data;
-        console.log(characters);
-        setCharData(characters);
-      })
-      .catch(err => {console.log(err);});
-    },[]);
+    axios.get('https://swapi.co/api/people/1')
+    .then (res => {
+      const characters = res.data;
+      console.log(characters);
+      setCharData(characters);
+    })
+    .catch(err => {console.log(err);});
+  },[]);
+
+  // return(
+  //   <div className='character'>
+  //     {charData.map(character => {
+  //       return(
+  //         <CharacterCard
+  //         name = {charData.name}
+  //         gender={charData.gender}
+  //         height={charData.height}
+  //         mass={charData.mass}
+  //         />
+  //         );
+  //       })
+  //     } 
+  //   </div>
+  // )
 
   return(
     <div className='character'>
-      <CharacterCard name = {charData.name} gender={charData.gender} height={charData.height} mass={charData.mass} />
+      <CharacterCard
+      name = {charData.name}
+      gender={charData.gender}
+      height={charData.height}
+      mass={charData.mass}
+      />
     </div>
   )
 }
